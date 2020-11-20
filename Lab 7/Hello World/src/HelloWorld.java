f7080e8ff8207b4312511c54c3671ee0985e49be
Humza <70176575+Humza2710@users.noreply.github.com>
2020-11-20 03:50:36 +0300

    Lab 10 Commit

--- Lab 7/Hello World/src/HelloWorld.java
+++ Lab 7/Hello World/src/HelloWorld.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+import java.lang.Math;
 
 public class HelloWorld {
 
@@ -5,6 +7,31 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world, this is Humza!");
 
+		//Lab 10:
+		
+		Scanner scnr = new Scanner(System.in);
+	      double wallHeight;
+	      double wallWidth;
+	      double wallArea;
+	      double paintNeeded;
+	      double cans;
+	      int cansInt;
+	      
+	      System.out.println("Enter wall height (feet):");
+	      wallHeight = scnr.nextDouble();
+	      
+	      System.out.println("Enter wall width (feet):");
+	      wallWidth = scnr.nextDouble();
+	      
+	      wallArea = wallWidth*wallHeight;
+	      System.out.println("Wall area: " + wallArea + " square feet");
+	      
+	      paintNeeded = wallArea/350;
+	      System.out.println("Paint needed: " + paintNeeded + " gallons");
+	      
+	      cansInt = (int) Math.ceil(paintNeeded);
+	      System.out.println("Cans needed: " + cansInt + " can(s)");
+		
 	}
 
 }
